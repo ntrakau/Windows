9122089800eaffff67a11547f37c2177d6ed41bb
ntrakau <ntrakau@gmail.com>
2023-12-19 17:31:10 -0800

    first commit

---
+++ kofi_assignment.py
@@ -0,0 +1,14 @@
+#creating a basic calculator
+
+#initialize the number in a input format
+Number1 = int(input("Enter the first number: "))
+Number2 = int (input("Enter the second number: "))
+total = 0
+#the math 
+addTotal = Number1 + Number2
+multiplyTotal = Number1 * Number2
+divideTotal = Number1 / Number2
+subtractTotal = Number1 - Number2 
+#the logic
+
+print(f"The total for Number1 and Number2 is: {total}")
